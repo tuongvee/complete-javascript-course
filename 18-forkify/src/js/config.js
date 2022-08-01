cbeda5de9230e76193cff1110633b997183a5fd0
tuongvee <18520403@gm.uit.edu.vn>
2022-08-01 23:15:12 +0700

    complete section 18

---
+++ 18-forkify/src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '1d9311fc-2248-46e0-b468-0305eedc03ba';
+export const MODAL_CLOSE_SEC = 2.5;
